11e361de93ed4c6b9c63386188eca32cd7144b76
joceth <eljoc@hotmail.com>
2015-07-10 12:54:03 -0500

    creacion de mantenimineto de descuentos de la aplicacion pagos

--- apps/pagos/urls.py
+++ apps/pagos/urls.py
@@ -5,13 +5,28 @@ urlpatterns = [
     url(
         r'^panel/admin/descuento/$',
         views.DescuentoView.as_view(),
-        name='descuento_panel'
+        name='panel_descuento'
 
     ),
-   url(
+    url(
         r'^panel/admin/descuento/agregar/$',
         views.AgregarDescuento.as_view(),
         name='agregar_descuento'
+    ),
+    url(
+        r'^panel/admin/descuento/detalle/(?P<pk>\d+)/$',
+        views.DetalleDescuento.as_view(),
+        name='detalle_descuento'
+    ),   
+    url(
+        r'^panel/admin/descuento/modificar/(?P<pk>\d+)/$',
+        views.ModificarDescuento.as_view(),
+        name='modificar_descuento'
+    ),
+    url(
+        r'^panel/admin/descuento/eliminar/(?P<pk>\d+)/$',
+        views.EliminarDescuento.as_view(),
+        name='eliminar_descuento'
     ),
     url(
         r'^panel/admin/estructurapagos/$',
--- apps/pagos/views.py
+++ apps/pagos/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView
+from django.views.generic import TemplateView, DetailView, CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
-from .forms import *
+
+from .forms import DescuentoForm
 from .models import Descuento
 
 
@@ -15,10 +16,31 @@ class DescuentoView(TemplateView):
 	    return context
 
 
+class DetalleDescuento(DetailView):
+    template_name = 'mantenimientos/descuento/detalle_descuento.html'
+    model = Descuento
+
+
+class AgregarDescuento(CreateView):
+    form_class = DescuentoForm
+    template_name = 'mantenimientos/descuento/agregar_descuento.html'
+    success_url = reverse_lazy('pagos_app:panel_descuento')
+
+
+class ModificarDescuento(UpdateView):
+    model = Descuento
+    template_name = 'mantenimientos/descuento/modificar_descuento.html'
+    success_url = reverse_lazy('pagos_app:panel_descuento')
+    form_class = DescuentoForm
+
+
+class EliminarDescuento(DeleteView):
+    template_name = 'mantenimientos/descuento/eliminar_descuento.html'
+    model = Descuento
+    success_url = reverse_lazy('pagos_app:panel_descuento')
+
+
+
 class EstructurapagoView(TemplateView):
 	template_name = 'mantenimientos/estructurapagos/estructurapagos_panel.html'
     
-class AgregarDescuento(CreateView):
-    form_class = DescuentoForm
-    template_name = 'mantenimientos/descuento/agregar.html'
-    success_url = reverse_lazy('inicio')
