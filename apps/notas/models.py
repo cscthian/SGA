59aab7366428fef36fcce6d4e51bc617aeb179a9
henrry <090229@unsaac.edu.pe>
2015-07-17 10:25:07 -0500

    cracion de asistencia alumno

--- apps/notas/models.py
+++ apps/notas/models.py
@@ -3,30 +3,16 @@ from django.template.defaultfilters import slugify
 from apps.asistencia.models import Docente
 
 
-class Modulo(models.Model):
-    nombre = models.CharField('nombre', max_length=50)
-    carrera = models.ForeignKey('matricula.Carrera')
-
-    class meta:
-        verbose_name_plural = 'Modulos'
-        ordering = ['nombre']
-
-    def __unicode__(self):
-        return self.nombre
-
-
 class Asignatura(models.Model):
     TIPO_CATEGORIA_CHOICES = (
         ('obligatorio', 'obligatorio'),
         ('opcional', 'opcional'),
     )
-    codigo = models.CharField(max_length=4, unique=True)
+    codigo = models.CharField(max_length=10, unique=True)
     nombre = models.CharField(max_length=50)
     categoria = models.CharField(max_length=15, choices=TIPO_CATEGORIA_CHOICES)
-    creditos = models.PositiveIntegerField(default=0)
-    modulo = models.ForeignKey(Modulo)
-    horas_teoricas = models.PositiveIntegerField(default=0)
-    horas_practicas = models.PositiveIntegerField(default=0)
+    horas_teorica = models.PositiveIntegerField(default=0)
+    horas_practica = models.PositiveIntegerField(default=0)
     slug = models.SlugField(editable=False)
 
     def save(self, *args, **kwargs):
@@ -38,13 +24,26 @@ class Asignatura(models.Model):
         return self.nombre
 
 
+class Modulo(models.Model):
+    nombre = models.CharField('nombre', max_length=50)
+    carrera = models.ForeignKey('matricula.Carrera')
+    asignatura = models.ManyToManyField(Asignatura)
+
+    class Meta:
+        verbose_name_plural = 'Modulos'
+        ordering = ['nombre']
+
+    def __unicode__(self):
+        return self.nombre
+
+
 class nota(models.Model):
     matricula = models.ForeignKey('matricula.Matricula')
     docente = models.ForeignKey(Docente)
     asignatura = models.ForeignKey(Asignatura)
     programacion = models.ForeignKey('matricula.Programacion')
-    nota1 = models.CharField('PP1', max_length=20, default='--')
-    nota2 = models.CharField('PP2', max_length=20, default='--')
-    nota3 = models.CharField('PP3', max_length=20, default='--')
+    nota1 = models.CharField('PP1', max_length=20, default='NSP')
+    nota2 = models.CharField('PP2', max_length=20, default='NSP')
+    nota3 = models.CharField('PP3', max_length=20, default='NSP')
     nota4 = models.CharField('PP4', max_length=20, default='--')
     promedio = models.DecimalField(max_digits=5, decimal_places=2)
