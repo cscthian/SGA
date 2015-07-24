662c85d31024e7b618447b1a40d6a30f52ee384c
dgafz <scam1121@gmail.com>
2015-07-24 18:27:47 -0500

    paginacion

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,4 +1,12 @@
+# -*- encoding: utf-8 -*-
 from django.contrib import admin
 from .models import User
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+	
+	list_display = ('username','first_name','last_name')
+	fieldsets = (
+		('User',{'fields': ('username','password')}),
+		('Personal Info',{'fields': ('first_name','last_name')}),
+	)
